9f218bc72c66958d7de057f438e5791537b67880
rogeliofernandezdev <rogelio.fernandez.aybar@gmail.com>
2026-09-20 16:22:38 -0700

    feat(kitchen): expose KDS board

--- foods-admin-web/src/modules/operations/index.ts
+++ foods-admin-web/src/modules/operations/index.ts
@@ -1,3 +1,5 @@
 export {OrdersManager} from "./orders/presentation/orders-manager";
 export {SalonManager} from "./salon/presentation/salon-manager";
 export {TablesManager} from "./tables/presentation/tables-manager";
+
+export {KitchenBoard} from "./kitchen/presentation/kitchen-board";
