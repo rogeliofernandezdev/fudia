4984328f25ef155b78a4e9e09524ec4de88a136d
rogeliofernandezdev <rogelio.fernandez.aybar@gmail.com>
2026-09-21 16:13:13 -0400

    feat(recipes): export recipe module

--- foods-admin-web/src/modules/menu/index.ts
+++ foods-admin-web/src/modules/menu/index.ts
@@ -1,3 +1,5 @@
 export {CatalogManager} from "./products/presentation/catalog-manager";
 export {ProductAvailabilityManager} from "./availability/presentation/product-availability-manager";
 export {CombosPage} from "./combos/presentation/combos-page";
+
+export {RecipesPage} from "./recipes/presentation/recipes-page";
