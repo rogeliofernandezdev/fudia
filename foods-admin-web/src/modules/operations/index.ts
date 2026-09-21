98c268e0e083a3d18cfb1eefd32fc46794d0cb49
rogeliofernandezdev <rogelio.fernandez.aybar@gmail.com>
2026-09-21 00:23:03 -0700

    feat(cash): export cash workspace

--- foods-admin-web/src/modules/operations/index.ts
+++ foods-admin-web/src/modules/operations/index.ts
@@ -3,3 +3,4 @@ export {SalonManager} from "./salon/presentation/salon-manager";
 export {TablesManager} from "./tables/presentation/tables-manager";
 
 export {KitchenBoard} from "./kitchen/presentation/kitchen-board";
+export {CashPage} from "./cash/presentation/cash-page";
