6653cd8f97f699c985889cda72381b3597830fa3
rogeliofernandezdev <rogelio.fernandez.aybar@gmail.com>
2026-09-22 09:35:27 -0400

    feat(admin): export reservations page

--- foods-admin-web/src/modules/operations/index.ts
+++ foods-admin-web/src/modules/operations/index.ts
@@ -5,3 +5,5 @@ export {TablesManager} from "./tables/presentation/tables-manager";
 export {KitchenBoard} from "./kitchen/presentation/kitchen-board";
 export {CashPage} from "./cash/presentation/cash-page";
 export {POSPage} from "./pos/presentation/pos-page";
+
+export {ReservationsPage} from "./reservations/presentation/reservations-page";
