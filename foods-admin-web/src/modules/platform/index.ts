30524533506c1abd3098cc098cd7658071728b16
rogeliofernandezdev <rogelio.fernandez.aybar@gmail.com>
2026-09-22 11:52:35 -0400

    feat(admin): export platform billing surfaces

--- foods-admin-web/src/modules/platform/index.ts
+++ foods-admin-web/src/modules/platform/index.ts
@@ -1,3 +1,4 @@
 export {PlatformOnboardingPage} from "./presentation/platform-onboarding-page";
-
+export {PlatformPlansPage} from "./presentation/platform-plans-page";
 export {PlatformShell} from "./presentation/platform-shell";
+export type {OrganizationSubscription,SubscriptionPlan,SubscriptionPayment} from "./domain/types";
