f33ca7bf6ff198a41ffaca212a1c0772ebf491d8
rogeliofernandezdev <rogelio.fernandez.aybar@gmail.com>
2026-09-24 15:59:30 -0400

    refactor(concierge): unify routing decisions

--- fudia-concierge/src/domain/intents.py
+++ fudia-concierge/src/domain/intents.py
@@ -1,11 +1,25 @@
 from typing import Literal
 
 AgentIntent = Literal["menu", "order", "service"]
+ConciergeRoute = Literal[
+    "out_of_scope",
+    "menu",
+    "order",
+    "service",
+]
 
 INTENT_MENU: AgentIntent = "menu"
 INTENT_ORDER: AgentIntent = "order"
 INTENT_SERVICE: AgentIntent = "service"
 INTENTS: tuple[AgentIntent, ...] = (
+    INTENT_MENU,
+    INTENT_ORDER,
+    INTENT_SERVICE,
+)
+
+ROUTE_OUT_OF_SCOPE: ConciergeRoute = "out_of_scope"
+ROUTES: tuple[ConciergeRoute, ...] = (
+    ROUTE_OUT_OF_SCOPE,
     INTENT_MENU,
     INTENT_ORDER,
     INTENT_SERVICE,
